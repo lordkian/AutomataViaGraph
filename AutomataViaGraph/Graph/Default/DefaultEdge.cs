508d2bd7ab5dbb1aa8ba06dc4b7c651de564b9e2
kian kazem <lordkian2000@gmail.com>
2019-09-24 23:47:08 +0330

    adding DefaultVertex and DefaultEdge

---
+++ AutomataViaGraph/Graph/Default/DefaultEdge.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutomataViaGraph.Graph.Default
+{
+    public class DefaultEdge<T> : IEdge<T>
+    {
+        public IVertex<T> From { get { return _from; } set { _from = value; } }
+        public IVertex<T> To { get { return _to; } set { _to = value; } }
+        private IVertex<T> _from, _to;
+    }
+}
---
+++ AutomataViaGraph/Graph/Default/DefaultVertex.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutomataViaGraph.Graph.Default
+{
+    public class DefaultVertex<T> : IVertex<T>
+    {
+        public T Value { get { return _value; } set { _value = value } }
+        private T _value;
+        public IEnumerable<IEdge<T>> Edges { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    }
+}
