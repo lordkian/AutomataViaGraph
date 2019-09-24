994341a8781d7fc65e1cedd84ea38a2c9dd59940
kian kazem <lordkian2000@gmail.com>
2019-09-24 20:35:58 +0330

    adding IGraph

---
+++ AutomataViaGraph/Graph/IGraph.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutomataViaGraph.Graph
+{
+    public interface IGraph<T>
+    {
+        IEnumerable<IEdge<T>> Edges { get; }
+        IEnumerable<IVertex<T>> Vertices { get; }
+    }
+}
