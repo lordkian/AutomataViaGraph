22c5eae11155e7afaa79399a8714f0759e7f9043
kian kazem <lordkian2000@gmail.com>
2019-09-24 23:33:05 +0330

    adding UndirectedGraph

---
+++ AutomataViaGraph/Graph/Default/UndirectedGraph.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutomataViaGraph.Graph.Default
+{
+    public class UndirectedGraph<T> : IGraph<T>
+    {
+        public IEnumerable<IEdge<T>> Edges { get { return edges; } }
+        public IEnumerable<IVertex<T>> Vertices { get { return vertices; } }
+        private List<IEdge<T>> edges = new List<IEdge<T>>();
+        private List<IVertex<T>> vertices = new List<IVertex<T>>();
+        public void Add(T data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddEdge(T vertex1, T vertex2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(T data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
