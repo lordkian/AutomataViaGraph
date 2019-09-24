5af5f41881568bba9ca927651e91dc88e6ce76e8
kian kazem <lordkian2000@gmail.com>
2019-09-25 00:09:15 +0330

    implmenting Add and Remove

--- AutomataViaGraph/Graph/Default/UndirectedGraph.cs
+++ AutomataViaGraph/Graph/Default/UndirectedGraph.cs
@@ -6,13 +6,16 @@ namespace AutomataViaGraph.Graph.Default
 {
     public class UndirectedGraph<T> : IGraph<T>
     {
-        public IEnumerable<IEdge<T>> Edges { get { return edges; } }
-        public IEnumerable<IVertex<T>> Vertices { get { return vertices; } }
-        private List<IEdge<T>> edges = new List<IEdge<T>>();
-        private List<IVertex<T>> vertices = new List<IVertex<T>>();
+        public IEnumerable<IEdge<T>> Edges { get { return _edges; } }
+        public IEnumerable<IVertex<T>> Vertices { get { return _vertices; } }
+        private List<IEdge<T>> _edges = new List<IEdge<T>>();
+        private List<IVertex<T>> _vertices = new List<IVertex<T>>();
         public void Add(T data)
         {
-            throw new NotImplementedException();
+            foreach (var item in _vertices)
+                if (item.Value.Equals(data))
+                    throw new ArgumentException("An item with the same value has already been added.");
+            _vertices.Add(new DefaultVertex<T>() { Value = data });
         }
 
         public void AddEdge(T vertex1, T vertex2)
@@ -21,6 +24,17 @@ namespace AutomataViaGraph.Graph.Default
         }
 
         public void Remove(T data)
+        {
+            IVertex<T> removeItem = null;
+            foreach (var item in _vertices)
+                if (item.Value.Equals(data))
+                    removeItem = item;
+            if (removeItem == null)
+                throw new ArgumentException("An item with this value does not exist.");
+            _vertices.Remove(removeItem);
+        }
+
+        public void RemoveEdge(T vertex1, T vertex2)
         {
             throw new NotImplementedException();
         }
