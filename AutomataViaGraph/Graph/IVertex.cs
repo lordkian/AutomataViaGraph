4b8a41bdfb20f239d549cc56cf57ebbd7be973a0
kian kazem <lordkian2000@gmail.com>
2019-09-24 20:31:53 +0330

    adding IVertex

---
+++ AutomataViaGraph/Graph/IVertex.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutomataViaGraph.Graph
+{
+    public interface IVertex<T>
+    {
+        T Value { get; set; }
+
+    }
+}
