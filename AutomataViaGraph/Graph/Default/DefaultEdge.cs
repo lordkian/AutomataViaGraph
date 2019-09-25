ffd3b3930e20a0f9fa8d1c4c599a0752704d63f2
kian kazem <lordkian2000@gmail.com>
2019-09-25 12:40:59 +0330

    overriding object methods in DefaultEdge

--- AutomataViaGraph/Graph/Default/DefaultEdge.cs
+++ AutomataViaGraph/Graph/Default/DefaultEdge.cs
@@ -9,5 +9,19 @@ namespace AutomataViaGraph.Graph.Default
         public IVertex<T> From { get { return _from; } set { _from = value; } }
         public IVertex<T> To { get { return _to; } set { _to = value; } }
         private IVertex<T> _from, _to;
+        public override bool Equals(object obj)
+        {
+            return obj is DefaultEdge<T> &&
+                (((obj as DefaultEdge<T>)._from.Equals(_from) && (obj as DefaultEdge<T>)._to.Equals(_to)) ||
+                 ((obj as DefaultEdge<T>)._to.Equals(_from) && (obj as DefaultEdge<T>)._from.Equals(_to)));
+        }
+        public override string ToString()
+        {
+            return _from.ToString() + "\n" + _to.ToString();
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
     }
 }
