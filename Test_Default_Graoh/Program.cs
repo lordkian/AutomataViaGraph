9ff7f0497b243481ffa2962d2ab7f84e487dd533
kian kazem <lordkian2000@gmail.com>
2019-09-25 10:09:41 +0330

    adding Test Default Graph Console app

---
+++ Test_Default_Graoh/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Test_Default_Graph
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
