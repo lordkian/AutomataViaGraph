b4e5c22cc4b2ff9cb33bb3f1271983704aad89ac
kian kazem <lordkian2000@gmail.com>
2019-10-01 16:53:49 +0330

    adding Name to IGraph

--- AutomataViaGraph/Graph/IGraph.cs
+++ AutomataViaGraph/Graph/IGraph.cs
@@ -9,7 +9,9 @@ namespace AutomataViaGraph.Graph
         IEnumerable<IEdge<T>> Edges { get; }
         IEnumerable<IVertex<T>> Vertices { get; }
         void Add(T data);
+        void Add(T data, string name);
         void Remove(T data);
+        void AddEdge(T vertex1, T vertex2, string name);
         void AddEdge(T vertex1, T vertex2);
         void RemoveEdge(T vertex1, T vertex2);
     }
